6ad5fe924fa4cd2c63347995079bc3d9e3886802
CarlosV-work <carlos.vep.work@gmail.com>
2026-09-15 22:48:13 -0400

    feat: agregado totalConsumo

--- src/main.cpp
+++ src/main.cpp
@@ -28,10 +28,12 @@
 // configuración interruptor baño
 #define SWITCH_BANIO 32
 
+const int LUZ_W = 12;
+int totalConsumo = 0;
 void setup() {
   Serial.begin(115200);
-  //pinMode(LED_TV_PIN, OUTPUT);
 
+  // asignando los switchs a los pines
   pinMode(SWITCH_DORMITORIO, INPUT_PULLUP);
   pinMode(SWITCH_COCINA, INPUT_PULLUP);
   pinMode(SWITCH_SALA, INPUT_PULLUP);
@@ -39,6 +41,7 @@ void setup() {
 }
 
 void loop() {
+  // obteniendo valores de los potenciómetros
   uint32_t tv = analogRead(TV_PIN);
   uint32_t lavadora = analogRead(LAVADORA_PIN);
   uint32_t radio = analogRead(RADIO_PIN);
@@ -66,18 +69,27 @@ void loop() {
 
   // monitorización de consumo eléctrico de la dormitorio
   int switchDormitorio = digitalRead(SWITCH_DORMITORIO);
+  int luzDormitorio = switchDormitorio ? LUZ_W:0;
   Serial.println(switchDormitorio? "Luz de dormitorio encendido":"Dormitorio apagado");
 
   // monitorización de consumo eléctrico de la cocina
   int switchCocina = digitalRead(SWITCH_COCINA);
+  int luzCocina = switchCocina? LUZ_W:0;
   Serial.println(switchCocina? "Luz de cocina encendido":"Cocina apagada");
   
   // monitorización de consumo eléctrico de la sala
   int switchSala = digitalRead(SWITCH_SALA);
-  Serial.println(switchSala? "Luz de sala encendido":"Sala apagado");
+  int luzSala = switchSala? LUZ_W:0;
+  Serial.println(switchSala? "Luz de sala encendida":"Sala apagado");
 
   // monitorización de consumo eléctrico del baño
   int switchBanio = digitalRead(SWITCH_BANIO);
+  int luzBanio = switchBanio? LUZ_W:0;
   Serial.println(switchBanio? "Luz de baño encendido":"Baño apagado");
-  delay(500);
+
+  totalConsumo = totalConsumo + ledTV + ledLavadora + ledRadio + ledFreezer + luzDormitorio + luzSala + luzCocina + luzBanio;
+  float consumoEnKW = static_cast<float>(totalConsumo) / 1000;
+  Serial.println("Consumo por segundo en kW: "+String(consumoEnKW)); // para totalConsumo 372 sale 0.37
+
+  delay(1000);
 }
